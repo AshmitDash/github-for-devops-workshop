642440ebec4c77f660b740c2754dd155580d07b7
AshmitDash <ashmit.dash@gmail.com>
2025-02-16 20:40:41 +0530

    modified code

--- program1.py
+++ program1.py
@@ -1,3 +1,7 @@
-a = input("First no,:")
-b = input("Second no.:")
-print(max(a, b))
+"""
+This module demonstrates how to find the maximum of two numbers.
+"""
+
+a = input("First no,: ")
+b = input("Second no.: ")
+print(max(a, b))  # max() works on strings, but should ideally be on numbers
